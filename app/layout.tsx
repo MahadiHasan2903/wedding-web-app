3dbbc0e7a1c9d8e450ef268b221e8f20dac08a6d
Mahadi Hasan <12mahadihasan@gmail.com>
2025-07-12 13:04:59 +0600

    chor(layout): update import statements order

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 

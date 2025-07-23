97453d5294a0a62b75a17ff7864f8b066eb60ac7
Mahadi Hasan <12mahadihasan@gmail.com>
2025-07-23 15:28:23 +0600

    feat(auth): added middleware functionalities for protecting routes

--- middleware.tsx
+++ middleware.tsx
@@ -1,5 +1,17 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/messages/:path*",
+    "/overview/:path*",
+    "/my-profile/:path*",
+    "/blocked-users/:path*",
+    "/liked-profiles/:path*",
+    "/user-management/:path*",
+    "/admin-management/:path*",
+    "/report-management/:path*",
+    "/pricing-management/:path*",
+    "/recommended-matches/:path*",
+    "/subscription-payment/:path*",
+  ],
 };

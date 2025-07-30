eacdd6a24895a8257b3e674f1b7788f570866de6
Mahadi Hasan <12mahadihasan@gmail.com>
2025-07-30 15:09:02 +0600

    feat(profile): implemented funcitonality to update user profile picture

--- lib/features/dashboard/user/profile/basic-info/BasicInfo.tsx
+++ lib/features/dashboard/user/profile/basic-info/BasicInfo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import Link from "next/link";
 import {
   getStateNameFromIso,
@@ -27,6 +27,7 @@ import BasicInfoUpdateForm from "./BasicInfoUpdateForm";
 import vipRing from "@/public/images/common/vip-ring.png";
 import { ImageWithFallback } from "@/lib/components/image";
 import { calculateAgeFromDOB } from "@/lib/utils/dateUtils";
+import { updateUserProfileAction } from "@/lib/action/user/user.action";
 import userPlaceholder from "@/public/images/common/user-placeholder.png";
 import { updateLikeDisLikeStatusAction } from "@/lib/action/user/userInteraction.action";
 
@@ -55,11 +56,20 @@ const BasicInfo = ({
   const router = useRouter();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [previewImageUrl, setPreviewImageUrl] = useState<string | null>(null);
 
   const { data: session } = useSession();
   const membershipId =
     session?.user.data.purchasedMembership?.membershipPackageInfo?.id;
   const isVipUser = membershipId !== undefined && [2, 3].includes(membershipId);
+
+  useEffect(() => {
+    return () => {
+      if (previewImageUrl) {
+        URL.revokeObjectURL(previewImageUrl);
+      }
+    };
+  }, [previewImageUrl]);
 
   // Function to update like/dislike status
   const handleUpdateLikeStatus = async () => {
@@ -88,15 +98,58 @@ const BasicInfo = ({
     setLoading(false);
   };
 
+  // Function to update profile picture
+  const handleUpdateProfilePicture = async (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const previewUrl = URL.createObjectURL(file);
+      setPreviewImageUrl(previewUrl);
+      console.log("Selected profile image:", file);
+
+      const formData = new FormData();
+      formData.append("profilePicture", file);
+
+      setLoading(true);
+
+      const updateProfileResponse = await updateUserProfileAction(formData);
+
+      toast(updateProfileResponse.message, {
+        type: updateProfileResponse.status ? "success" : "error",
+      });
+
+      if (updateProfileResponse.status) {
+        router.refresh();
+      }
+
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-full h-full flex flex-col xl:flex-row items-start xl:items-center gap-[20px] lg:gap-[40px] xl:gap-[100px] bg-white rounded-none lg:rounded-[10px] px-[18px] py-[12px] lg:px-[36px] lg:py-[20px]">
       <div className="w-auto shrink-0 flex items-center gap-[16px]">
-        <div className="w-[90px] lg:w-[150px] h-[90px] lg:h-[160px] relative flex items-center justify-center">
+        <label
+          className={`${
+            loading ? "cursor-not-allowed" : "cursor-pointer"
+          } w-[90px] lg:w-[150px] h-[90px] lg:h-[160px] relative flex items-center justify-center`}
+          htmlFor={editable ? "profilePictureUpload" : undefined}
+        >
+          <input
+            type="file"
+            id="profilePictureUpload"
+            accept="image/*"
+            hidden
+            onChange={handleUpdateProfilePicture}
+            disabled={!editable || loading}
+          />
+
           <div className="w-[85px] lg:w-[145px] h-[85px] lg:h-[145px] relative overflow-hidden">
             <ImageWithFallback
               fill
               alt="user"
-              src={userProfile.profilePicture?.url}
+              src={previewImageUrl || userProfile.profilePicture?.url}
               fallBackImage={userPlaceholder}
               className="rounded-full object-cover border border-black"
             />
@@ -112,7 +165,8 @@ const BasicInfo = ({
               />
             </div>
           )}
-        </div>
+        </label>
+
         <div className="flex flex-col items-start gap-1 lg:gap-[15px]">
           <div>
             <h2 className="text-[14px] lg:text-[20px] font-medium text-primary">
